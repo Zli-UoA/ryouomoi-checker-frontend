386f4f3f7e8ce6af360e1089b96a9d45e47e372f
eraser5th <hisanniwo0202@gmail.com>
2022-02-15 20:27:51 +0900

    #30:HakyokuPage done

---
+++ src/pages/HakyokuPage/HakyokuPage.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import HakyokuPage from './HakyokuPage';
+
+export default {
+  title: 'Example/HakyokuPage',
+  component: HakyokuPage,
+} as ComponentMeta<typeof HakyokuPage>;
+
+// eslint-disable-next-line
+const Template: ComponentStory<typeof HakyokuPage> = (args) => <HakyokuPage {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
---
+++ src/pages/HakyokuPage/HakyokuPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HakyokuPage: React.VFC = () => <div>HakyokuPage</div>;
+
+export default HakyokuPage;
