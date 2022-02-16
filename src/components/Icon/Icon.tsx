f2a635444f2f817a4ff3ff0f91851b2ad78f28ef
eraser5th <hisanniwo0202@gmail.com>
2022-02-16 23:36:48 +0900

    #36:add delete icon

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -68,3 +68,9 @@ export const AddIcon: React.VFC = () => (
     <path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z" fill="#2C2E5C" />
   </svg>
 );
+
+export const DeleteIcon: React.VFC = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M16 9V19H8V9H16ZM14.5 3H9.5L8.5 4H5V6H19V4H15.5L14.5 3ZM18 7H6V19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7Z" fill="#2C2E5C" />
+  </svg>
+);
