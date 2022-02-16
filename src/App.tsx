a964c5d47732ac470ce9adbb7540b2310b092d03
eraser5th <hisanniwo0202@gmail.com>
2022-02-16 12:06:11 +0900

    #30:fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import HomePage from './pages/HomePage/HomePage';
 import LostPartnerPage from './pages/LostPartnerPage/LostPartnerPage';
 import PowerWordPage from './pages/PowerWordPage/PowerWordPage';
 import SearchPage from './pages/SearchPage/SearchPage';
-import TolkRoomPage from './pages/TolkRoomPage/TolkRoomPage';
+import TalkRoomPage from './pages/TalkRoomPage/TalkRoomPage';
 import TutorialPage from './pages/TutorialPage/TutorialPage';
 import WelcomePage from './pages/WelcomePage/WelcomePage';
 
@@ -44,7 +44,7 @@ const App: React.VFC = () => (
       <Route path="/lost-partner" element={<LostPartnerPage />} />
       <Route path="/power-word" element={<PowerWordPage />} />
       <Route path="/search" element={<SearchPage />} />
-      <Route path="/tolkroom" element={<TolkRoomPage />} />
+      <Route path="/tolkroom" element={<TalkRoomPage />} />
       <Route path="/welcome" element={<WelcomePage />} />
     </Routes>
   </div>
