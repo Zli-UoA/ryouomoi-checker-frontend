5b96d5ca213c45a8bf2d79f2be316e90410837d3
matumoto1234 <matumotosizuoka@gmail.com>
2022-02-18 04:13:40 +0900

    #39:Create page2 tsx

---
+++ src/pages/TutorialPage/Page2.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from '../../components/Button/Button';
+import '../../common.css';
+import './Page2.css';
+
+const Page2: React.VFC = () => (
+  <div className="page2">
+    <div className="page2__text">
+      <p>
+        {/* 1行目 */}
+        5段階で
+        <span className="color_primarySolid">
+          好き度
+        </span>
+        を登録し
+        <br />
+
+        {/* 2行目 */}
+        好き度の合計が6以上になった
+        <br />
+
+        {/* 3行目 */}
+        1人とマッチします
+      </p>
+    </div>
+    <div className="page2__button">
+      <Button label="Twitterで登録" />
+    </div>
+  </div>
+);
+
+export default Page2;
