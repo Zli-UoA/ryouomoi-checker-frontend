dbfa55958eccbad797cb7a4208ab07b6c58141c2
eraser5th <hisanniwo0202@gmail.com>
2022-02-18 02:12:05 +0900

    #47:create WelcomePage

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import Button from '../../components/Button/Button';
+import '../../common.css';
+import './welcomePage.css';
+import WithBackground from '../../components/WithBackground/WithBackground';
+import UserIcon from '../../components/UserIcon/UserIcon';
 
-const WelcomePage: React.VFC = () => <div>WelcomePage</div>;
+const Page1: React.VFC = () => (
+  <div className="welcomePage">
+    <div className="">
+      <WithBackground>
+        <>
+          <div className="welcomePage__icon">
+            <UserIcon
+              size="lg"
+              image="https://pbs.twimg.com/profile_images/1429604062127792132/4JPTr6M9_400x400.jpg"
+            />
+          </div>
+          <p className="welcomePage__name">会津太郎</p>
+          <p className="welcomePage__id">@aizu_taro</p>
+        </>
+      </WithBackground>
+    </div>
+    <h1 className="welcomePage__h1">ようこそ</h1>
+    <div className="welcomePage__button">
+      <Button label="恋愛の一歩を踏み出す" />
+    </div>
+  </div>
+);
 
-export default WelcomePage;
+export default Page1;
