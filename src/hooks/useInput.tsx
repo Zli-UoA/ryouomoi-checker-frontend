faba1fd3b183487964f4f6c5479a88887cfcbbd6
eraser5th <hisanniwo0202@gmail.com>
2022-02-17 23:51:59 +0900

    #49:useInputがplaceholderを受け取る様に変更

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -1,16 +1,16 @@
 import React, { useRef } from 'react';
 
-type UseInput = (className: string) => {
+type UseInput = (className: string, placeholder: string) => {
   inputRef: React.RefObject<HTMLInputElement>,
   Input: React.VFC,
 };
 
-const useInput: UseInput = (className) => {
+const useInput: UseInput = (className, placeholder) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   return {
     inputRef,
-    Input: () => <input className={className} ref={inputRef} />,
+    Input: () => <input className={className} ref={inputRef} placeholder={placeholder} />,
   };
 };
 
