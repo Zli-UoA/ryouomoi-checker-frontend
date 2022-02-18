1dd6a772e6d4a2ed7e2a779004968b49a2a480df
eraser5th <hisanniwo0202@gmail.com>
2022-02-18 20:45:54 +0900

    #65:Cut out the main content

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -5,6 +5,50 @@ import useSearchInput from '../../components/Input/useSearchInput';
 import '../../common.css';
 import UserIcon from '../../components/UserIcon/UserIcon';
 
+type User = {
+  name: string,
+  image: string,
+  id: string
+};
+
+type SearchPageMainContentProps = {
+  users: User[]
+};
+
+const SearchPageMainContent: React.VFC<SearchPageMainContentProps> = ({ users }) => {
+  if (users.length === 0) {
+    return (
+      <div style={{ paddingTop: '200px' }}>
+        <div>
+          <span className="color_primarysolid">検索ボックス</span>
+          に
+          <br />
+          キーワードを入力してください
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div style={{ paddingTop: '200px' }}>
+      {
+        users.map(({ name, image, id }) => (
+          <div style={{ display: 'flex' }}>
+            <UserIcon
+              size="sm"
+              image={image}
+            />
+            <div>
+              <h3>{name}</h3>
+              <p>{id}</p>
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  );
+};
+
 const SearchPage: React.VFC = () => {
   const { SearchInput } = useSearchInput();
   return (
@@ -17,28 +61,15 @@ const SearchPage: React.VFC = () => {
           </>
         </Header>
       </div>
-      <div style={{ paddingTop: '200px' }}>
-        <div>
-          <span className="color_primarySolid">検索ボックス</span>
-          に
-          <br />
-          キーワードを入力してください
-        </div>
-        {
-          new Array(100).fill(
-            <div style={{ display: 'flex' }}>
-              <UserIcon
-                size="sm"
-                image="https://pbs.twimg.com/profile_images/1429604062127792132/4JPTr6M9_400x400.jpg"
-              />
-              <div>
-                <h3>会津夏那子</h3>
-                <p>@kanako</p>
-              </div>
-            </div>,
-          )
-        }
-      </div>
+      <SearchPageMainContent
+        users={(
+          new Array(Math.random() < 0.5 ? 0 : 30).fill({
+            name: '会津夏菜子',
+            image: 'https://pbs.twimg.com/profile_images/1429604062127792132/4JPTr6M9_400x400.jpg',
+            id: '@kanako',
+          })
+        )}
+      />
     </div>
   );
 };
