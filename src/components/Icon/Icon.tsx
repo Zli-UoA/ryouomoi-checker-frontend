1b6f58f16cde201f94a0f72ada75a391048d21e1
matumoto1234 <matumotosizuoka@gmail.com>
2022-02-18 22:30:12 +0900

    #64:Implement SearchIcon

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -47,9 +47,7 @@ export const BackIcon: React.VFC = () => (
 );
 
 export const SearchIcon: React.VFC = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M15.5 14H14.71L14.43 13.73C15.41 12.59 16 11.11 16 9.5C16 5.91 13.09 3 9.5 3C5.91 3 3 5.91 3 9.5C3 13.09 5.91 16 9.5 16C11.11 16 12.59 15.41 13.73 14.43L14 14.71V15.5L19 20.49L20.49 19L15.5 14ZM9.5 14C7.01 14 5 11.99 5 9.5C5 7.01 7.01 5 9.5 5C11.99 5 14 7.01 14 9.5C14 11.99 11.99 14 9.5 14Z" fill="#2C2E5C" />
-  </svg>
+  <SearchIconSvg />
 );
 
 export const AddIcon: React.VFC = () => (
@@ -57,9 +55,7 @@ export const AddIcon: React.VFC = () => (
 );
 
 export const DeleteIcon: React.VFC = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M16 9V19H8V9H16ZM14.5 3H9.5L8.5 4H5V6H19V4H15.5L14.5 3ZM18 7H6V19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7Z" fill="#2C2E5C" />
-  </svg>
+  
 );
 
 export const CheckIcon: React.VFC = () => (
