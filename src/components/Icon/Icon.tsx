e97febcd6e3295a4ddb1e691cc1e1f7563517841
matumoto1234 <matumotosizuoka@gmail.com>
2022-02-18 22:31:23 +0900

    #64:Implement SendIcon

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -63,7 +63,5 @@ export const CheckIcon: React.VFC = () => (
 );
 
 export const SendIcon: React.VFC = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M2.01 21L23 12L2.01 3L2 10L17 12L2 14L2.01 21Z" fill="#2C2E5C" />
-  </svg>
+  <SendIconSvg />
 );
