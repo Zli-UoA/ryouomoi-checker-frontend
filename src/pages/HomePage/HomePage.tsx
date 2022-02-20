e307f27b904e8693f4bd9e9ae1364f8f3df77b3a
matumoto1234 <matumotosizuoka@gmail.com>
2022-02-21 06:19:43 +0900

    #63:Create home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,81 @@
 import React from 'react';
-import useHeartRating from '../../components/HeartRating/useHeartRating';
+import { Link } from 'react-router-dom';
 import '../../common.css';
+import './homePage.css';
+import Header from '../../components/Header/Header';
+import AddButton from '../../components/AddButton/AddButton';
+import UserIcon from '../../components/UserIcon/UserIcon';
 
-const HomePage: React.VFC = () => {
-  const { HeartRating } = useHeartRating();
+const HomePageHeader: React.VFC = () => (
+  <Header>
+    <div className="homePage__header">
+      <Link to="/search">
+        <AddButton />
+      </Link>
 
-  return (
-    <div className="bg_primary">
-      <HeartRating />
+      <div>
+        <div className="homePage__title">
+          両想いチェッカー
+        </div>
+      </div>
+
+      <div>
+        <UserIcon image="https://pbs.twimg.com/profile_images/1429604062127792132/4JPTr6M9_400x400.jpg" size="xs" />
+      </div>
     </div>
-  );
-};
+  </Header>
+);
+
+const HomePageContent: React.VFC = () => (
+  <>
+    {/* 空のdivだが、ヘッダーが position: fixed なためヘッダー分(64px)を調整 */}
+    <div style={{ height: '64px' }} />
+    <div className="mg_top-80">
+      <div className="homePage__text">
+        <p>
+          左上の「＋」から
+
+          <br />
+
+          <span className="homePage__secondaryText">
+            好きな相手
+          </span>
+
+          を登録しよう
+        </p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        a
+      </div>
+    </div>
+  </>
+);
+
+const HomePage: React.VFC = () => (
+  <>
+    <HomePageHeader />
+    <HomePageContent />
+  </>
+);
 
 export default HomePage;
