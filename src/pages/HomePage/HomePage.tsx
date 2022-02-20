0305b0e7472a3076b56f545354d8731d7c1a640b
eraser5th <hisanniwo0202@gmail.com>
2022-02-21 00:27:04 +0900

    #81:ログイン後welcomeページに遷移する様にした

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import useHeartRating from '../../components/HeartRating/useHeartRating';
 import '../../common.css';
+import useQuery from '../../hooks/useQuery';
+
+const useNavigateToWelcome = (): void => {
+  const navigate = useNavigate();
+  const query = useQuery();
+  useEffect(() => {
+    if (query.get('auth_token')) {
+      navigate('/welcome');
+    }
+  }, [query]);
+};
 
 const HomePage: React.VFC = () => {
   const { HeartRating } = useHeartRating();
+  useNavigateToWelcome();
 
   return (
     <div className="bg_primary">
