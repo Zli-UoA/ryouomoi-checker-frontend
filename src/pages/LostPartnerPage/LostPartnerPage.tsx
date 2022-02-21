f9373e033f47ed43e46da0992d63043263a83f79
matumoto1234 <matumotosizuoka@gmail.com>
2022-02-22 05:02:06 +0900

    #66:Remove LostPartnerPage contents

--- src/pages/LostPartnerPage/LostPartnerPage.tsx
+++ src/pages/LostPartnerPage/LostPartnerPage.tsx
@@ -1,23 +1,5 @@
-import React, { useState } from 'react';
-import usePopup from '../../hooks/usePopup/usePopup';
+import React from 'react';
 
-const LostPartnerPage: React.VFC = () => {
-  const [isOpening, setOpening] = useState(false);
-  const { Popup } = usePopup();
-
-  type VoidFunction = () => void;
-  const openPopup: VoidFunction = () => {
-    setOpening(true);
-  };
-
-  return (
-    <div>
-      <button type="button" onClick={openPopup}>
-        Click
-      </button>
-      <Popup isOpening={isOpening} setOpening={setOpening} />
-    </div>
-  );
-};
+const LostPartnerPage: React.VFC = () => <div>LostPartnerPage</div>;
 
 export default LostPartnerPage;
