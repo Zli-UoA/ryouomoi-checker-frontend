e15736685d0be96e386e2cc855f5ca1e535eb1dd
matumoto1234 <matumotosizuoka@gmail.com>
2022-02-21 18:46:15 +0900

    #66:Add usePopup test

--- src/pages/LostPartnerPage/LostPartnerPage.tsx
+++ src/pages/LostPartnerPage/LostPartnerPage.tsx
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import usePopup from '../../hooks/usePopup/usePopup';
 
-const LostPartnerPage: React.VFC = () => <div>LostPartnerPage</div>;
+const LostPartnerPage: React.VFC = () => {
+  const [isOpening, setOpening] = useState(false);
+  const { Popup } = usePopup();
+
+  type VoidFunction = () => void;
+  const openPopup: VoidFunction = () => {
+    setOpening(true);
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={openPopup}>
+        Click
+      </button>
+      <Popup isOpening={isOpening} setOpening={setOpening} />
+    </div>
+  );
+};
 
 export default LostPartnerPage;
