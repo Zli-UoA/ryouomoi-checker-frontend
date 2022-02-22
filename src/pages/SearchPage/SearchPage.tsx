2eac5e7172ccccf621acae9517977b364bd2738f
eraser5th <hisanniwo0202@gmail.com>
2022-02-23 06:21:46 +0900

    #108:fix

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import { BackIcon } from '../../components/Icon/Icon';
 import useSearchInput, { OnEnter } from '../../components/Input/useSearchInput';
