b9c09f3720c1fb676146037f640972b7d89dc0e2
matumoto1234 <matumotosizuoka@gmail.com>
2022-02-25 00:26:21 +0900

    #66:Add selectedCount in props

--- src/hooks/useLovePoint/useInitializedHeartRating.tsx
+++ src/hooks/useLovePoint/useInitializedHeartRating.tsx
@@ -3,13 +3,14 @@ import useHeartRating, { ValidNumber } from '../../components/HeartRating/useHea
 
 const baseURL = 'http://localhost:8080';
 
-type UseGetLovePoint = (id: string) => {
+type UseInitializedHeartRating = (id: string) => {
   error: Error | undefined,
   lovePoint: ValidNumber,
-  HeartRating: React.VFC;
+  selectedCount: ValidNumber,
+  HeartRating: React.VFC,
 };
 
-const useGetLovePoint: UseGetLovePoint = (id: string) => {
+const useInitializedHeartRating: UseInitializedHeartRating = (id: string) => {
   type DataType = {
     lovePoint: number
   };
@@ -20,50 +21,56 @@ const useGetLovePoint: UseGetLovePoint = (id: string) => {
     headers: new Headers({ Authorization: `Bearer ${token}` }),
   });
 
-  const { HeartRating: HeartRating1 } = useHeartRating(1);
-  const { HeartRating: HeartRating2 } = useHeartRating(2);
-  const { HeartRating: HeartRating3 } = useHeartRating(3);
-  const { HeartRating: HeartRating4 } = useHeartRating(4);
-  const { HeartRating: HeartRating5 } = useHeartRating(5);
+  const { selectedCount: selectedCount1, HeartRating: HeartRating1 } = useHeartRating(1);
+  const { selectedCount: selectedCount2, HeartRating: HeartRating2 } = useHeartRating(2);
+  const { selectedCount: selectedCount3, HeartRating: HeartRating3 } = useHeartRating(3);
+  const { selectedCount: selectedCount4, HeartRating: HeartRating4 } = useHeartRating(4);
+  const { selectedCount: selectedCount5, HeartRating: HeartRating5 } = useHeartRating(5);
 
   switch (data?.lovePoint) {
     case 1:
       return {
         error,
         lovePoint: 1,
+        selectedCount: selectedCount1,
         HeartRating: HeartRating1,
       };
     case 2:
       return {
         error,
         lovePoint: 2,
+        selectedCount: selectedCount2,
         HeartRating: HeartRating2,
       };
     case 3:
       return {
         error,
         lovePoint: 3,
+        selectedCount: selectedCount3,
         HeartRating: HeartRating3,
       };
     case 4:
       return {
         error,
         lovePoint: 4,
+        selectedCount: selectedCount4,
         HeartRating: HeartRating4,
       };
     case 5:
       return {
         error,
         lovePoint: 5,
+        selectedCount: selectedCount5,
         HeartRating: HeartRating5,
       };
     default:
       return {
         error,
         lovePoint: 1,
+        selectedCount: selectedCount1,
         HeartRating: HeartRating1,
       };
   }
 };
 
-export default useGetLovePoint;
+export default useInitializedHeartRating;
