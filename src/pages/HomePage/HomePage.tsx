388a34602dbef93c78fa275751f8402a8c7da673
matumoto1234 <matumotosizuoka@gmail.com>
2022-02-25 08:43:34 +0900

    #82:Remove TODO comment

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -76,9 +76,6 @@ const HomePageContent: React.VFC = () => {
 
   return (
     <>
-      {/* TODO: ここに/me/lovers/が空であれば↓、空でなければUserListを表示するようにする */}
-      {/* TODO: useFetch で /me/lovers/ をたたく. UserData[] を返すだけ */}
-      {/* TODO: これで、if(data) と if(data.ユーザーリストが空) で分岐するだけになる */}
       {/* 空のdivだが、ヘッダーが position: fixed なためヘッダー分(64px)を調整 */}
       <div style={{ height: '64px' }} />
       <div className="mg_top-80">
