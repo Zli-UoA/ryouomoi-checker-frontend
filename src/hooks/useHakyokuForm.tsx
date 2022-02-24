d09f9e8f8747533901ce18ad901ccdd397a8af2c
eraser5th <hisanniwo0202@gmail.com>
2022-02-24 17:40:30 +0900

    #120:create useHakyokuForm

---
+++ src/hooks/useHakyokuForm.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import RadioButton from '../components/RadioButton/RadioButton';
+
+type UseHakyokuForm = () => {
+  reason: string | undefined;
+  selected: boolean;
+  HakyokuForm: React.VFC;
+};
+
+const useHakyokuForm: UseHakyokuForm = () => {
+  const [reason, setReason] = useState<string | undefined>(undefined);
+
+  const genSetReason = (target: string) => () => setReason(target);
+
+  const reasons = [
+    '価値観の違い',
+    '趣味・趣向の違い',
+    '冷めた・嫌いになった',
+    '他に好きな人ができた',
+    '自然消滅',
+    'その他',
+  ];
+
+  const HakyokuForm: React.VFC = () => (
+    <div className="hakyokuForm">
+      <h1 className="hakyokuForm__title">
+        破局理由を教えてください
+      </h1>
+      {
+        reasons.map((r) => (
+          <RadioButton
+            label={r}
+            onClick={genSetReason(r)}
+            selected={r === reason}
+          />
+        ))
+      }
+    </div>
+  );
+
+  return {
+    reason, selected: reason !== undefined, HakyokuForm,
+  };
+};
+
+export default useHakyokuForm;
