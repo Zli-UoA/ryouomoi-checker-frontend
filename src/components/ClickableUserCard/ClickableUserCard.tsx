326408c849fef997e6b8ad653d2d80922303e838
eraser5th <hisanniwo0202@gmail.com>
2022-02-26 02:40:44 +0900

    #165:create clickableUserCard

---
+++ src/components/ClickableUserCard/ClickableUserCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import UserCard from '../../pages/SearchPage/UserCard';
+import User from '../../types/User';
+import './clickableUserCard.css';
+
+type ClickableUserCardProps = {
+  user: User,
+  onClick: () => void
+};
+
+const ClickableUserCard: React.VFC<ClickableUserCardProps> = ({ user, onClick }) => (
+  <button type="button" onClick={onClick} className="clickableUserCard">
+    <UserCard
+      displayName={user.displayName}
+      imageUrl={user.imageUrl}
+      screenName={user.screenName}
+    />
+  </button>
+);
+
+export default ClickableUserCard;
