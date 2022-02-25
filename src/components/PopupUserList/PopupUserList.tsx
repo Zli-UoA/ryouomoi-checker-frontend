8e522a6b367f1182c20c7b1afd672af4c3e836e3
eraser5th <hisanniwo0202@gmail.com>
2022-02-26 03:00:34 +0900

    #165:refact PopupUserList

--- src/components/PopupUserList/PopupUserList.tsx
+++ src/components/PopupUserList/PopupUserList.tsx
@@ -1,20 +1,62 @@
-import React from 'react';
-import PopupUserCard from '../PopupUserCard/PopupUserCard';
-import { User } from '../../pages/SearchPage/UserCard';
+import React, { useState } from 'react';
+import useOpen from '../../hooks/useOpen';
+import useHeartRating from '../../hooks/useHeartRating';
+import Popup from '../Popup/Popup';
+import ClickableUserCard from '../ClickableUserCard/ClickableUserCard';
+import User from '../../types/User';
 
-export type UserCardsInfo = {
-  user: User,
-  id: string,
-}[];
+export type UserCardsInfo = User[];
 
 type PopupUserListProps = {
   userCardsInfo: UserCardsInfo
 };
 
-const PopupUserList: React.VFC<PopupUserListProps> = ({ userCardsInfo }) => (
-  <div>
-    {userCardsInfo.map(({ user, id }) => <PopupUserCard user={user} id={id} />)}
-  </div>
-);
+const PopupUserList: React.VFC<PopupUserListProps> = ({ userCardsInfo }) => {
+  const { isOpen, close, open } = useOpen();
+
+  const [whichUser, setWhichUser] = useState<User>({
+    imageUrl: 'dummyUrl',
+    id: 'dummyId',
+    screenName: 'dummyScreenName',
+    displayName: 'dummyDisplayName',
+  });
+
+  const { rating, setRating, clearRating } = useHeartRating();
+
+  const genOnCardClick = (user: User) => (): void => {
+    setWhichUser(user);
+    open();
+  };
+
+  const primaryAction = (): void => {
+    close();
+  };
+
+  const cancelAction = (): void => {
+    close();
+    clearRating();
+  };
+
+  const deleteAction = (): void => {
+    close();
+  };
+
+  return (
+    <div>
+      {userCardsInfo.map((user) => (
+        <ClickableUserCard user={user} onClick={genOnCardClick(user)} />
+      ))}
+      <Popup
+        user={whichUser}
+        isOpen={isOpen}
+        heartRating={rating}
+        setHeartRating={setRating}
+        primaryAction={primaryAction}
+        cancelAction={cancelAction}
+        deleteAction={deleteAction}
+      />
+    </div>
+  );
+};
 
 export default PopupUserList;
