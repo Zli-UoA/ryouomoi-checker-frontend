a60873c4fde90d3d295a425e1617235dd24567be
matumoto1234 <matumotosizuoka@gmail.com>
2022-02-25 18:16:25 +0900

    #147:Create makeMessage function

--- src/hooks/useDialog/useDialog.tsx
+++ src/hooks/useDialog/useDialog.tsx
@@ -7,7 +7,7 @@ type VoidFunction = () => void;
 type ReasonId = 1 | 2 | 3 | 4 | 5 | 6;
 
 type DialogProps = {
-  hakyokuMonths: number,
+  days: number,
   reasonId: ReasonId,
   arrowShare: boolean,
 };
@@ -38,7 +38,23 @@ const useDialog: UseDialog = () => {
     });
   };
 
-  const Dialog: React.VFC<DialogProps> = ({ reasonId, arrowShare }) => {
+  const makeMessage = (days: number): string => {
+    if (days < 7) {
+      return 'え？何かの間違いだよね？';
+    }
+    if (days < 30) {
+      return `${days}日もたってないよ？`;
+    }
+    if (days < 30 * 6) {
+      return '半年持たなかったんですか？';
+    }
+    if (days < 30 * 12) {
+      return `たった${Math.floor(days / 30)}ヶ月で？`;
+    }
+    return `${Math.floor(days / (30 * 12))}年も付き合っておいて？`;
+  };
+
+  const Dialog: React.VFC<DialogProps> = ({ days, reasonId, arrowShare }) => {
     if (!isOpen) return null;
 
     return (
@@ -52,6 +68,9 @@ const useDialog: UseDialog = () => {
                   破局
                 </span>
                 したんですか？
+
+                <br />
+                {makeMessage(days)}
               </p>
             </div>
           </div>
