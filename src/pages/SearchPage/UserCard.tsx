e6e0f06cc0ba86302c254c93b94d9bb2408760d0
matumoto1234 <matumotosizuoka@gmail.com>
2022-02-25 10:55:01 +0900

    #82:Trimming too long name

--- src/pages/SearchPage/UserCard.tsx
+++ src/pages/SearchPage/UserCard.tsx
@@ -8,16 +8,25 @@ type User = {
   screenName: string
 };
 
+const trimming = (tooLongName: string, limit: number): string => {
+  let res = tooLongName;
+  if (tooLongName.length >= limit) {
+    const trimmed = tooLongName.substring(0, limit);
+    res = `${trimmed}...`;
+  }
+  return res;
+};
+
 const UserCard: React.VFC<User> = ({
   displayName, imageUrl, screenName,
 }) => (
   <div className="userCard">
-    <div className="userCard__icon">
+    <div className="userCard__icon mg_top-8 mg_bottom-8 mg_left-8 mg_right-16">
       <UserIcon size="sm" image={imageUrl} />
     </div>
-    <div className="userCard__text">
-      <h1 className="userCard__name">{displayName}</h1>
-      <h2 className="userCard__id">{screenName}</h2>
+    <div className="userCard__text mg_top-8 mg_bottom-8 mg_right-8">
+      <h1 className="userCard__name">{trimming(displayName, 15)}</h1>
+      <h2 className="userCard__id">{trimming(screenName, 40)}</h2>
     </div>
   </div>
 );
