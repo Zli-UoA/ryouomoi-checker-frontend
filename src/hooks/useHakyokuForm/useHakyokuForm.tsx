b9c0fdcccef6d06c04ca79b2290a1c3f9671e497
matumoto1234 <matumotosizuoka@gmail.com>
2022-02-25 13:09:33 +0900

    #137:Add ReasonId at return value

--- src/hooks/useHakyokuForm/useHakyokuForm.tsx
+++ src/hooks/useHakyokuForm/useHakyokuForm.tsx
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import RadioButton from '../../components/RadioButton/RadioButton';
 import './useHakyokuForm.css';
 
+export type ReasonId = 1 | 2 | 3 | 4 | 5 | 6;
+
 type UseHakyokuForm = () => {
   reason: string | undefined;
+  reasonId: ReasonId | undefined,
   selected: boolean;
   HakyokuForm: React.VFC;
 };
@@ -21,6 +24,25 @@ const useHakyokuForm: UseHakyokuForm = () => {
     '自然消滅',
     'その他',
   ];
+
+  const toReasonId = (r: string | undefined): ReasonId => {
+    switch (r) {
+      case reasons[0]:
+        return 1;
+      case reasons[1]:
+        return 2;
+      case reasons[2]:
+        return 3;
+      case reasons[3]:
+        return 4;
+      case reasons[4]:
+        return 5;
+      case reasons[5]:
+        return 6;
+      default:
+        return 1;
+    }
+  };
 
   const HakyokuForm: React.VFC = () => (
     <div className="hakyokuForm">
@@ -42,7 +64,10 @@ const useHakyokuForm: UseHakyokuForm = () => {
   );
 
   return {
-    reason, selected: reason !== undefined, HakyokuForm,
+    reason,
+    reasonId: toReasonId(reason),
+    selected: reason !== undefined,
+    HakyokuForm,
   };
 };
 
