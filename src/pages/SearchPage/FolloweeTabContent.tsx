05fa9c10f29f0cda00b4f97b06b2616a3cbd9b67
matumoto1234 <matumotosizuoka@gmail.com>
2022-02-25 14:46:32 +0900

    #132:Attach Popup UserList in FolloweeTabContent

--- src/pages/SearchPage/FolloweeTabContent.tsx
+++ src/pages/SearchPage/FolloweeTabContent.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useFetch } from 'usehooks-ts';
-import UserList from './UserList';
+import PopupUserList, { UserCardsInfo } from '../../components/PopupUserList/PopupUserList';
 
 const FolloweeTabContent: React.VFC = () => {
   const token = localStorage.getItem('ryouomoi-checker-token');
 
   const { data } = useFetch<{
+    id: string,
     displayName: string
     imageUrl: string
     screenName: string
@@ -13,9 +14,36 @@ const FolloweeTabContent: React.VFC = () => {
     headers: new Headers({ Authorization: `Bearer ${token}` }),
   });
 
+  if (data === undefined) {
+    return (
+      <main className="searchPage__main">
+        <PopupUserList userCardsInfo={[]} />
+      </main>
+    );
+  }
+
+  const userCardsInfo: UserCardsInfo = new Array(data.length);
+  for (let i = 0; i < data.length; i += 1) {
+    const {
+      displayName,
+      imageUrl,
+      screenName,
+      id,
+    } = data[i];
+    userCardsInfo[i] = {
+      user: {
+        displayName,
+        imageUrl,
+        screenName,
+      },
+      id,
+      mode: 'Add',
+    };
+  }
+
   return (
     <main className="searchPage__main">
-      <UserList users={data || []} />
+      <PopupUserList userCardsInfo={userCardsInfo} />
     </main>
   );
 };
