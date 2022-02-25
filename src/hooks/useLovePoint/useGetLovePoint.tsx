b493c152d86a1b404dd3e7df762a93c870d27908
eraser5th <hisanniwo0202@gmail.com>
2022-02-26 01:54:29 +0900

    #165:修正

--- src/hooks/useLovePoint/useGetLovePoint.tsx
+++ src/hooks/useLovePoint/useGetLovePoint.tsx
@@ -3,18 +3,16 @@ import toValidNumber from '../../lib/toValidNumber';
 import ValidNumber from '../../types/ValidNumber';
 import useFetchWithAuth from '../useFetchWithAuth';
 
+type DataType = {
+  lovePoint: ValidNumber
+};
+
 type UseGetLovePoint = (id: string) => {
   lovePoint: ValidNumber,
   error: Error | undefined,
 };
 
 const useGetLovePoint: UseGetLovePoint = (id: string) => {
-  type DataType = {
-    lovePoint: number
-  };
-
-  console.log(id);
-
   const { data, error } = useFetchWithAuth<DataType>(`${baseURL}/me/lovers/${id}`);
 
   let lovePoint: ValidNumber = 1;
