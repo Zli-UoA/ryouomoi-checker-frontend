a223ee2fdd48e3b7f81e9f1246ce2995a018a2f4
eraser5th <hisanniwo0202@gmail.com>
2022-02-25 22:13:57 +0900

    #162:refact useFetchWithAuth

--- src/hooks/useFetchWithAuth.ts
+++ src/hooks/useFetchWithAuth.ts
@@ -2,10 +2,18 @@ import { useState, useEffect } from 'react';
 import JSONbig from 'json-bigint';
 import getToken from '../lib/getToken';
 
-const useFetchWithAuth = <T>(url: string): {
-  data: T | undefined;
-  error: Error | undefined;
-} => {
+type Init = {
+  method: 'POST' | 'DELETE' | 'GET',
+  body: string,
+};
+
+const useFetchWithAuth = <T>(
+  url: string,
+  init?: Init,
+): {
+    data: T | undefined;
+    error: Error | undefined;
+  } => {
   const [data, setResponse] = useState<T | undefined>(undefined);
   const [error, setError] = useState<Error | undefined>(undefined);
   const token = getToken();
@@ -14,14 +22,13 @@ const useFetchWithAuth = <T>(url: string): {
     const doFetch = async (): Promise<void> => {
       try {
         const res = await fetch(url, {
+          ...init,
           headers: new Headers({ Authorization: `Bearer ${token}` }),
         });
 
         const text = await res.text();
 
         const json: T = JSONbig({ storeAsString: true }).parse(text);
-
-        console.log('json-bigint result', json);
 
         setResponse(json);
       } catch (e) {
@@ -31,7 +38,7 @@ const useFetchWithAuth = <T>(url: string): {
     };
 
     doFetch();
-  }, [setError, setResponse, url, token]);
+  }, [init, setError, setResponse, url, token]);
 
   return { data, error };
 };
