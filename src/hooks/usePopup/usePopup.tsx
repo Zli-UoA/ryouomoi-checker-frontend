700d2f635c4c423dd3651ce2bacc931bea24bd31
eraser5th <hisanniwo0202@gmail.com>
2022-02-25 23:21:52 +0900

    #165:refact usePopup

--- src/hooks/usePopup/usePopup.tsx
+++ src/hooks/usePopup/usePopup.tsx
@@ -2,77 +2,81 @@ import React from 'react';
 import './usePopup.css';
 import '../../common.css';
 import UserIcon from '../../components/UserIcon/UserIcon';
-import { ValidNumber } from '../../components/HeartRating/useHeartRating';
-import usePopupTrigger, { PopupTriggerProps } from './usePopupTrigger';
-import useLovePoint from '../useLovePoint/useLovePoint';
+import useHeartRating, { ValidNumber } from '../../components/HeartRating/useHeartRating';
 import PopupButton from '../../components/PopupButton/PopupButton';
 import DeleteButton from '../../components/DeleteButton/DeleteButton';
 
-type UsePopup = (
+type User = {
   imageUrl: string,
   displayName: string,
   screenName: string,
   id: string,
-  mode: 'Add' | 'Edit'
-) => {
+  lovePoint?: number,
+};
+
+type UsePopupProps = {
+  user: User,
   isOpen: boolean,
+  primaryAction?: () => void,
+  deleteAction?: () => void,
+  cancelAction?: () => void,
+};
+
+type UsePopup = ({
+  user, isOpen, primaryAction, deleteAction, cancelAction,
+}: UsePopupProps) => {
   selectedCount: ValidNumber,
-  PopupTrigger: React.VFC<PopupTriggerProps>,
   Popup: React.VFC
 };
 
-const usePopup: UsePopup = (imageUrl, displayName, screenName, id, mode) => {
-  const {
-    postLovePoint,
-    lovePoint: oldLovePoint,
-    selectedCount,
-    clearCount,
-    HeartRating,
-  } = useLovePoint(id);
+const StatefulDeleteButton: React.VFC<{
+  user: User,
+  deleteAction: () => void
+}> = ({ user, deleteAction }) => {
+  if (!user.lovePoint) return null;
 
-  const {
-    isOpen,
-    closePopup,
-    PopupTrigger,
-  } = usePopupTrigger();
+  return (
+    <button type="button" className="popup__deleteButton" onClick={deleteAction}>
+      <DeleteButton />
+    </button>
+  );
+};
 
-  const StatefulDeleteButton: React.VFC = () => {
-    if (mode === 'Add') return null;
-
-    return (
-      <div className="popup__deleteButton">
-        <DeleteButton />
-      </div>
-    );
-  };
+const usePopup: UsePopup = ({
+  user, isOpen,
+  primaryAction = () => {},
+  deleteAction = () => {},
+  cancelAction = () => {},
+}) => {
+  const { selectedCount, HeartRating } = useHeartRating();
 
   const Popup: React.VFC = () => {
     if (!isOpen) return null;
 
     return (
-      <div className="popup__overlay" role="button" tabIndex={0} onClick={() => { closePopup(); clearCount(); }}>
+      <div className="popup__overlay" role="button" tabIndex={0} onClick={cancelAction}>
         <div className="popup__content" role="button" tabIndex={0} onClick={(e) => e.stopPropagation()}>
 
           <div className="mg_top-24">
             <div className="popup__userIcon">
-              <UserIcon size="md" image={imageUrl} />
+              <UserIcon size="md" image={user.imageUrl} />
             </div>
           </div>
 
-          <StatefulDeleteButton />
+          <StatefulDeleteButton user={user} deleteAction={deleteAction} />
 
           <div className="popup__commonFont">
             {/* displayName と userIcon の間が 12px なので、popup__displayName で margin-top を使っている */}
             <div className="popup__displayName">
-              {displayName}
+              {user.displayName}
             </div>
           </div>
 
           <div className="mg_top-8">
             <div className="popup__commonFont">
               <div className="popup__screenName">
-                <a href={`https://twitter.com/${screenName}`} target="_blank" rel="noreferrer">
-                  {screenName}
+                <a href={`https://twitter.com/${user.screenName}`} target="_blank" rel="noreferrer">
+                  {user.screenName}
                 </a>
               </div>
             </div>
@@ -84,12 +88,14 @@ const usePopup: UsePopup = (imageUrl, displayName, screenName, id, mode) => {
 
           <div className="mg_top-40">
             <div className="popup__buttonGroup">
-              <PopupButton label="キャンセル" onClick={() => { closePopup(); clearCount(); }} />
+              <PopupButton label="キャンセル" onClick={cancelAction} />
 
               <PopupButton
-                label={mode === 'Add' ? '好き！' : '更新'}
-                disabled={mode === 'Add' ? false : selectedCount === oldLovePoint}
-                onClick={() => { postLovePoint(selectedCount); closePopup(); clearCount(); }}
+                label={user.lovePoint ? '更新' : '好き！'}
+                disabled={user.lovePoint
+                  ? selectedCount === user.lovePoint
+                  : false}
+                onClick={primaryAction}
               />
             </div>
           </div>
@@ -99,9 +105,7 @@ const usePopup: UsePopup = (imageUrl, displayName, screenName, id, mode) => {
   };
 
   return {
-    isOpen,
     selectedCount,
-    PopupTrigger,
     Popup,
   };
 };
